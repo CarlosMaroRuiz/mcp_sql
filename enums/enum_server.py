c69828638c65978b5427810e67f58ffeaf2ea5ba
car06 <221220@ids.upchiapas.edu.mx>
2025-08-21 22:06:35 -0600

    new variables were added

--- enums/enum_server.py
+++ enums/enum_server.py
@@ -1,7 +1,13 @@
 #clase que contiene datos del servidor mcp como lo es nombre del server mcp y instrucciones
+from dotenv import load_dotenv
+import os
+load_dotenv()
 class EnumServer:
     def __init__(self):
-        self.NAME:str = "ConvertFiles"
+        self.NAME:str = "MCP_SQL"
         self.INSTRUCTIONS :str = "Por el momento no hay instrucciones"
+        self.HOST_SERVER = os.getenv("HOST_SERVER")
+        self.PORT_SERVER = os.getenv("PORT_SERVER")
+    
 
 enum_server = EnumServer()
--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from server.server_register import create_server
+from core import MySQLConnector
+from enums import enum_server
+import sys
 
 def main():
+    db = MySQLConnector()
+    if not db.validate_connection():
+        print("    Verifique que:")
+        print("    1. El servidor MySQL esté en ejecución")
+        print("    2. Las credenciales en el archivo .env sean correctas")
+        print("    3. La base de datos exista")
+        print("    4. El usuario tenga permisos adecuados")
+        sys.exit(1)
+    
     server = create_server()
-    server.run(transport="streamable-http")
-main()
+    
+    server.run(transport="streamable-http", host=enum_server.HOST_SERVER, port=int(enum_server.PORT_SERVER))
+
+if __name__ == "__main__":
+    main()
